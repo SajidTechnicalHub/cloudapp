6b61cc4ef5aa44726e02365615a3b39854b133cc
SajidTechnicalHub <sajid.mayoo@gmail.com>
2022-08-08 14:12:41 +0500

    add dropdown in Azure Inventory Details

--- src/components/dashboard/cloudVendors/AzureInventoryDetails.jsx
+++ src/components/dashboard/cloudVendors/AzureInventoryDetails.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from "react-router-dom"
 import TopBar from '../header/TopBar'
 
 const AzureInventoryDetails = () => {
-  return (
-    <TopBar subtitle='Azure / All VNets' />
-  )
+    const navigate = useNavigate();
+    const [cloudAccount, setCloudAccount] = useState({
+        cloud_account:'All Azure Cloud Accounts'
+        
+    })
+
+    const InputEvent = (e) => {
+        const { name, value } = e.target;
+        setCloudAccount(() => {
+            return { ...cloudAccount, [name]: value }
+        })
+    }
+    const SubmitEvent = (e) => {
+        e.preventDefault()
+
+    }
+    return (
+        <>
+            <TopBar subtitle='Azure / All VNets' />
+            <div className="azure-inventory-detail-container">
+                <div className="azure-inventory-detail-all-vnets-block">
+                    <span className="azure-inventory-detail-all-vnets-block-heading">
+                        All VNets (2)
+                    </span>
+                    <span className="azure-inventory-detail-all-vnets-block-dropdown">
+                        <select
+                            name="cloud_account"
+                            value={cloudAccount.cloud_account}
+                            onChange={InputEvent}
+                        >
+                            <option >All Azure Cloud Accounts</option>
+                            <option > Azure Accounts</option>
+
+                        </select>
+                    </span>
+                </div>
+            </div>
+
+        </>
+    )
 }
 
 export default AzureInventoryDetails
